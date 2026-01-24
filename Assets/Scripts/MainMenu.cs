c117a35cf70a182d3bb73a6b001308fecdb6ef4b
irentren <icalatravasierra@gmail.com>
2026-01-24 14:09:57 +0100

    Main menu!!

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine; 
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+               SceneManager.LoadSceneAsync("Main");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
